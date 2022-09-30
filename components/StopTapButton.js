ebee41b1f3541675ec8a66f31812b162709487cf
WajdiMuh <w.elmuhtadi@gju.edu.jo>
2022-09-30 03:38:13 +0300

    add disabled support for stoptap button

--- components/StopTapButton.js
+++ components/StopTapButton.js
@@ -1,17 +1,24 @@
 import React, { useRef, useEffect } from 'react';
 import { Animated, TouchableOpacity, Text, View } from 'react-native';
 export const StopTapButton: () => Node = (props) => {
+    const disabled = props.disabled === undefined ? false : props.disabled;
     return (
-        <TouchableOpacity style={{
-                borderWidth: 5,
+        <TouchableOpacity style={[{
+                borderWidth: 6,
                 borderColor: props.btcolor,
                 borderRadius: 5,
                 backgroundColor: props.bgcolor,
                 ...props.style
-            }} 
-            onPress={props.onPress}>
+            },disabled ? {opacity: 0} : {opacity: 1}]} 
+            onPress={props.onPress}
+            disabled={disabled}
+            >
             <View style={{alignItems: 'center',backgroundColor: props.bgcolor}}>
-                <Text style={{padding: 4, color: props.btcolor ,fontFamily: 'DotsAllForNowJL', fontSize: 17}}>{props.title}</Text>
+                {(props.children === undefined) ? (
+                    <Text style={{padding: 4, color: props.btcolor ,fontFamily: 'DotsAllForNowJL', fontSize: 17}}>{props.title}</Text>
+                ):(
+                    props.children
+                )}
             </View>
         </TouchableOpacity>
     );
