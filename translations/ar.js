a1a08841abdc10de05923c3852aea9a20a87c9d3
WajdiMuh <w.elmuhtadi@gju.edu.jo>
2022-09-30 03:39:07 +0300

    update translations

--- translations/ar.js
+++ translations/ar.js
@@ -7,5 +7,42 @@ export default {
     },
     general: {
         back: "عودة"
+    },
+    settings:{
+        title: "الإعدادات",
+        music: "الموسيقى",
+        sfx: "المؤثرات الصوتيه"
+    },
+    Languages:{
+        title: "اللغات"
+    },
+    dev:{
+        developedby: "طورت بواسطة",
+        name: "وجدي المهتدي",
+        thxforplaying: "شكراً على اللعب"
+    },
+    shop:{
+        buy: "اشتري",
+        select: "اختار",
+        selected: "مختار",
+        noenoughcoins: "ليس لديك\nعملات كافية"
+    },
+    shopitems:{
+        redandblue: "أحمر و أزرق",
+        greenandpink: "أخضر و وردي",
+        blueandorange: "أزرق و برتقالي",
+        purpleandgreen: "أرجواني و أخضر",
+        blueandyellow: "أزرق و أصفر",
+        brownandyellow: "بني و أصفر",
+        greyandred: "رمادي وأحمر",
+        lightblueandpink: "أزرق فاتح و وردي",
+        lightbrownandlightgreen: "بني فاتح و أخضر فاتح",
+        lightgreenandblue: "أخضر فاتح و أزرق",
+        oliveandorange: "زيتي و برتقالي",
+        lightpinkandfuschia: "وردي فاتح و فوشي"
+    },
+    languages: {
+        ar: "العربية\nمختار",
+        en: "إنجليزي"
     }
 } 
--- translations/en.js
+++ translations/en.js
@@ -7,5 +7,42 @@ export default {
     },
     general: {
         back: "Back"
+    },
+    settings:{
+        title: "Settings",
+        music: "Music",
+        sfx: "SFX"
+    },
+    Languages:{
+        title: "Languages"
+    },
+    dev:{
+        developedby: "Developed By",
+        name: "Wajdi ELMuhtadi",
+        thxforplaying: "Thanks For Playing"
+    },
+    shop:{
+        buy: "Buy",
+        select: "Select",
+        selected: "Selected",
+        noenoughcoins: "You Don't Have\nEnough Coins"
+    },
+    shopitems:{
+        redandblue: "Red And Blue",
+        greenandpink: "Green And Pink",
+        blueandorange: "Blue And Orange",
+        purpleandgreen: "Purple And Green",
+        blueandyellow: "Blue And Yellow",
+        brownandyellow: "Brown And Yellow",
+        greyandred: "Grey And Red",
+        lightblueandpink: "Light Blue And Pink",
+        lightbrownandlightgreen: "Light Brown And Light Green",
+        lightgreenandblue: "Light Green And Blue",
+        oliveandorange: "Olive And Orange",
+        lightpinkandfuschia: "Light Pink And Fuschia"
+    },
+    languages: {
+        ar: "Arabic",
+        en: "English\nSelected"
     }
 } 
