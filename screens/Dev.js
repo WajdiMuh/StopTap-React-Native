145ad8372d2fc57db3cff45d8ec1beb2b2ae4a11
WajdiMuh <w.elmuhtadi@gju.edu.jo>
2022-09-30 03:38:46 +0300

    add dev translation

--- screens/Dev.js
+++ screens/Dev.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
-import type {Node, useContext} from 'react';
+import type {Node} from 'react';
+import { useContext } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -17,24 +18,23 @@ import {useTheme} from '@react-navigation/native';
 import { Devimg } from '../components/Devimg';
 import { StopTapButton } from '../components/StopTapButton';
 import { Devname } from '../components/Devname';
-import { strings,langcontext } from '../translations/languages';
+import { strings } from '../translations/languages';
 
 export const Dev: () => Node = ({ navigation }) => {  
   const { colors } = useTheme();
   const styles = DevStyle(colors);
-  const {AppLang,SetAppLang} = useContext(langcontext);
   return (
     <View style={styles.container}>
-      <Text style={styles.textabove}>Developed By</Text>
+      <Text style={styles.textabove}>{strings.dev.developedby}</Text>
       <Devname style={styles.textabove} color={colors.text} interval={500}>
-        Wajdi ELMuhtadi
+        {strings.dev.name}
       </Devname>
       <Devimg>  
         <Image style={styles.devimg}
           source={require('../assets/imgs/wajdi.png')}
         />
       </Devimg>
-      <Text style={styles.textbelow}>Thanks for playing</Text>
+      <Text style={styles.textbelow}>{strings.dev.thxforplaying}</Text>
       <StopTapButton
         bgcolor={colors.background}
         btcolor={colors.text}
