0fea14ed8ddee854092544ab80713cff6caa1932
WajdiMuh <w.elmuhtadi@gju.edu.jo>
2022-11-03 09:13:21 +0100

    update assets and add splashscreen

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
  * @format
  * @flow
  */
-
+import 'react-native-gesture-handler';
 import React, { useEffect, useState, createContext } from 'react';
 import type {Node} from 'react';
 import {
@@ -13,7 +13,7 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { DarkTheme,DefaultTheme,NavigationContainer } from '@react-navigation/native'; 
 import { MainMenu } from './screens/MainMenu';
@@ -23,25 +23,49 @@ import { Settings } from './screens/Settings';
 import { Languages } from './screens/Languages';
 import { Shop } from './screens/Shop';
 import Toast from 'react-native-toast-message';
-import { toastConfig } from './components/Stoptaptoast'
+import { toastConfig } from './components/Stoptaptoast';
+import { shopanim,leaderboardanim,miscellaneousanim } from './screens/ScreenTransitions';
+import SplashScreen from 'react-native-splash-screen';
+import { GameOver } from './screens/GameOver';
+import API from './API/API';
+import { Leaderboard } from './screens/Leaderboard';
 export const themecontext = createContext();
 const App: () => Node = () => {
-  const Stack = createNativeStackNavigator();
+  const Stack = createStackNavigator();
   const scheme = useColorScheme();
   const [AppLang,SetAppLang] = useState("en");
   const [AppTheme,SetAppTheme] = useState('light');
+  useEffect(() => {
+    API.requestaccesstoken();
+    API.loaduserid();
+    API.loadusername();
+    SplashScreen.hide();
+  },[]);
+  const forFade = ({ current }) => ({
+    cardStyle: {
+      opacity: current.progress,
+    },
+  });
   return (
       <SafeAreaProvider style={{backgroundColor: AppTheme === 'dark' ? DarkTheme.colors.background : DefaultTheme.colors.background}}>
         <themecontext.Provider value={{AppTheme,SetAppTheme}}>
           <langcontext.Provider value={{AppLang,SetAppLang}}>
             <StatusBar hidden={true} />
               <NavigationContainer theme={AppTheme === 'dark' ? DarkTheme : DefaultTheme}>
-                <Stack.Navigator initialRouteName="MainMenu">
-                  <Stack.Screen name="MainMenu" component={MainMenu} options={{headerShown:false}}/>
-                  <Stack.Screen name="Dev" component={Dev} options={{headerShown:false}}/>
-                  <Stack.Screen name="Settings" component={Settings} options={{headerShown:false}}/>
-                  <Stack.Screen name="Languages" component={Languages} options={{headerShown:false}}/>
-                  <Stack.Screen name="Shop" component={Shop} options={{headerShown:false}}/>
+                <Stack.Navigator initialRouteName="MainMenu" screenOptions={{headerShown:false,cardStyleInterpolator: miscellaneousanim}} >
+                  <Stack.Screen name="MainMenu" component={MainMenu}/>
+                  <Stack.Screen name="Dev" component={Dev}/>
+                  <Stack.Screen name="Settings" component={Settings}/>
+                  <Stack.Screen name="Languages" component={Languages}/>
+                  <Stack.Screen name="Shop" component={Shop}
+                    options={{
+                      cardStyleInterpolator: shopanim
+                    }}/>
+                  <Stack.Screen name="Leaderboard" component={Leaderboard}
+                    options={{
+                      cardStyleInterpolator: leaderboardanim
+                    }}/>
+                  <Stack.Screen name="GameOver" component={GameOver}/>
                 </Stack.Navigator>
               </NavigationContainer>
           </langcontext.Provider>
--- screens/MainMenu.js
+++ screens/MainMenu.js
@@ -1,8 +1,7 @@
 // @flow
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Node, useState,useContext} from 'react';
 import {
-  SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
@@ -12,72 +11,128 @@ import {
   Image,
   TouchableOpacity,
 } from 'react-native';
+import Toast from 'react-native-toast-message';
 import {useTheme} from '@react-navigation/native'; 
 import { MainMenuTitle } from '../components/MainMenuTitle';
 import { strings,langcontext   } from '../translations/languages';
 import { StopTapButton } from '../components/StopTapButton';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import API from '../API/API';
+import { UsernameModal,UserNameModalVisiblity } from '../components/UsernameModal';
 
 export const MainMenu: () => Node = ({ navigation }) => {  
   const { colors } = useTheme();
   const styles = MainMenuStyle(colors);
   const {AppLang,SetAppLang} = useContext(langcontext);
+  const [UserNameModalVisible, setUserNameModalVisible] = useState(false);
   return (
-    <View style={styles.container}>
-      <MainMenuTitle style={styles.title}>
-        <Text style={styles.titletext}>StopTap</Text>
-      </MainMenuTitle>
-      <TouchableOpacity style={styles.devbutton} onPress={()=>{navigation.navigate('Dev')}}>
-        <Image
-          style={styles.devbuttonimg}
-          source={require('../assets/imgs/wajdi.png')}
+    <SafeAreaView style={styles.container}>
+      <UserNameModalVisiblity.Provider value={{UserNameModalVisible, setUserNameModalVisible}}>
+        <UsernameModal colors={colors} 
+          callback={(username) => {
+            API.setusername(username);
+          }}
         />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.settingsbutton} onPress={()=>{navigation.navigate('Settings')}}>
-        <Image
-          style={styles.settingsbuttonimg}
-          source={require('../assets/imgs/settings.png')}
+      </UserNameModalVisiblity.Provider>
+      <View style={styles.uppercontainer}>
+        <MainMenuTitle style={styles.title}>
+          <Text style={styles.titletext}>StopTap</Text>
+        </MainMenuTitle>
+        <TouchableOpacity style={styles.settingsbutton} onPress={()=>{
+          navigation.navigate('Settings');
+          Toast.hide();
+          }}>
+          <Image
+            style={styles.settingsbuttonimg}
+            source={require('../assets/imgs/settings.png')}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.midcontainer}>
+        <StopTapButton
+            bgcolor={colors.background}
+            btcolor={colors.text}
+            onPress={()=> {
+              Toast.hide();
+              navigation.pop();
+            }}
+            title={strings.mainmenu.play}
+            style={styles.gap}
         />
-      </TouchableOpacity>
-      <StopTapButton
+        <StopTapButton
           bgcolor={colors.background}
           btcolor={colors.text}
-          onPress={()=> {navigation.pop()}}
-          title={strings.mainmenu.play}
+          onPress={()=> {
+            Toast.hide();
+            navigation.pop();
+          }}
+          title={strings.mainmenu.howtoplay}
           style={styles.gap}
-      />
-      <StopTapButton
-        bgcolor={colors.background}
-        btcolor={colors.text}
-        onPress={()=> {navigation.pop()}}
-        title={strings.mainmenu.howtoplay}
-        style={styles.gap}
-      />
-      <StopTapButton
-        bgcolor={colors.background}
-        btcolor={colors.text}
-        onPress={()=> {navigation.navigate('Shop')}}
-        title={strings.mainmenu.shop}
-        style={styles.gap}
-      />
-      <StopTapButton
-        bgcolor={colors.background}
-        btcolor={colors.text}
-        onPress={()=> {navigation.pop()}}
-        title={strings.mainmenu.account}
-      />
-    </View>
+        />
+        <StopTapButton
+          bgcolor={colors.background}
+          btcolor={colors.text}
+          onPress={()=> {
+            Toast.hide();
+            navigation.navigate('Shop');
+          }}
+          title={strings.mainmenu.shop}
+          style={styles.gap}
+        />
+        <StopTapButton
+          bgcolor={colors.background}
+          btcolor={colors.text}
+          onPress={()=> {
+            if(API.checkuserid()){
+              Toast.hide();
+              navigation.navigate('Leaderboard');
+            }else if(!API.checkusername()){
+              setUserNameModalVisible(true);
+            }else{
+              Toast.show({
+                text1: "You need to set a highscore\nto be able to access the leaderboard",
+                topOffset: 50,
+                position: 'bottom',
+                visibilityTime: 2500,
+                props:{
+                    bgcolor: colors.text,
+                    textcolor: colors.background
+                }
+              });
+            }
+          }}
+          title={strings.mainmenu.leaderboard}
+        />
+      </View>
+      <View>
+        <TouchableOpacity style={styles.devbutton} onPress={()=>{
+          Toast.hide();
+          navigation.navigate('Dev');
+        }}>
+          <Image
+            style={styles.devbuttonimg}
+            source={require('../assets/imgs/wajdi.png')}
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const MainMenuStyle = (colors:any) => StyleSheet.create({
   container:{
+    flex: 1
+  },
+  uppercontainer:{
+    alignItems:'center'
+  },
+  midcontainer:{
     alignItems: 'center',
     flex: 1,
     justifyContent: 'center'
   },
   title:{
-    position:'absolute',
-    top: 40
+    top: 40,
   },
   titletext:{
     fontSize: 40,
